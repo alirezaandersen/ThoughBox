e6a55a0d5511c48e84d997abca02ca88f3d96ed3
Alireza Andersen <alireza.andersen@gmail.com>
2016-07-25 20:05:39 -0600

    refactor

--- app/assets/javascripts/sortLinks.js
+++ app/assets/javascripts/sortLinks.js
@@ -7,49 +7,6 @@ function filtersUnreadLinks(){
   $('.read-link').parent().show();
 }
 
-var count = 1;
-function sortByText(a, b) {
-  if (a.text < b.text) {
-    return - 1;
-  }
-  if (a.text > b.text) {
-    return 1;
-  }
-  return 0;
-}
-
-function addElements(elements){
-  for(let obj of elements){
-    $('.links').append(obj.element);
-  }
-}
-
-var original =[], sorted =[];
-function computeOriginal(){
-  original = $('.link').map(function () {
-    return {
-      element: this,
-      text: $(this).find('li').text()
-    };
-  });
-}
-
-function computeSorted(){
-  var temp = $.extend(true, {}, original);
-  sorted = Array.from(temp).sort(sortByText);
-}
-
-function sortAlphabetically(){
-  if (count === 1){
-    computeOriginal();
-  }
-
-  if( (count % 2) === 0){
-    addElements(original);
-  }
-  else{
-    computeSorted();
-    addElements(sorted);
-  }
-  ++count;
-}
+// function sortAlphabetically(){
+//
+// }
