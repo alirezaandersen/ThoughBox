11d242d2099a2ec25ac186e65e322978d80dcd6c
Alireza Andersen <alireza.andersen@gmail.com>
2016-07-27 13:04:50 -0600

    bug has been fix mark keeps state upon refresh

--- spec/features/user_can_edit_exisiting_link_spec.rb
+++ spec/features/user_can_edit_exisiting_link_spec.rb
@@ -1,48 +1,4 @@
 require "rails_helper"
-=begin
-RSpec.feature "Testing Selinum wait" do
-  scenario "they see links" do
-    user1 = create(:user)
-
-    visit '/login'
-
-    fill_in 'session_email', with: 'ali@gmail.com'
-    fill_in 'session_password', with: 'password'
-
-    click_on 'Log in'
-
-    fill_in :link_title, with: "Vacation Plans"
-    fill_in :link_url, with: "https://www.nps.gov/glac/index.htm"
-    click_on "Save Link"
-
-
-    driver = Selenium::WebDriver.for :firefox
-    #driver.get "/links"
-
-    visit "/links"
-    p "called visit links"
-    wait = Selenium::WebDriver::Wait.new(:timeout => 10)
-    begin
-      p "inside begin"
-      wait.until {driver.find_element(:class, "links")}
-    ensure
-      #Logger login error to text error log file
-      p "inside ensure"
-      p page
-      within (".links") do
-        expect(page).to have_content "https://www.nps.gov/glac/index.htm"
-      end
-      driver.quit #---------------------------> always goes to ensure and quits
-    end
-
-#    visit "links"
-
-#    click_on "Edit"
-  end
-
-end
-=end
-
 
 RSpec.feature "Authenticated user can create and save a link" do
   scenario "they see links" do
@@ -59,16 +15,12 @@ RSpec.feature "Authenticated user can create and save a link" do
     fill_in :link_url, with: "https://www.nps.gov/glac/index.htm"
     click_on "Save Link"
 
-    #visit "/links"
+    visit "/links"
 
-    #within (".links") do
-      #expect(page).to have_content "https://www.nps.gov/glac/index.htm"
-      
-      save_and_open_page
-      page.should have_content('https://www.nps.gov/glac/index.htm')
-    #end
-
-    visit "links"
+    page.should have_content('https://www.nps.gov/glac/index.htm')
+    page.should have_button('Edit')
+    page.should have_button('Mark as Unread')
+    page.should have_button('Mark as Read')
 
     click_on "Edit"
   end
